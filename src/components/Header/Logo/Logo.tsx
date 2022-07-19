29b2212d1d65584c9f3c6e5d1932ed8f2c17e665
Maciek <maciej.stepien1994@gmail.com>
2022-07-19 15:48:46 +0200

    Added Logo Component

---
+++ src/components/Header/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import { LogoContainer } from './Logo.styles'
+import React from 'react'
+
+export const Logo = () => {
+   return (
+      <LogoContainer>
+         <img
+            src="https://static1.s123-cdn-static-a.com/uploads/5191798/400_609bb5e2d9a39.png"
+            alt=""
+         />
+      </LogoContainer>
+   )
+}
