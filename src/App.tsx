85d7e54c6ee2f97b6abd1cbfa695535ba1c497ed
DevThomas <fremen1990@gmail.com>
2022-07-19 09:20:01 +0200

    Prettier, Eslint and Husky configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import React from 'react';
-import Home from "./pages/Home";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Login from "./pages/Login";
-import AvailableStudents from "./pages/AvailableStudents";
-import ForInterview from "./pages/ForInterview";
-import StudentPortfolio from "./pages/StudentPortfolio";
-import * as ROUTES from "./constants/routes"
+import React from 'react'
+import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import AvailableStudents from './pages/AvailableStudents'
+import ForInterview from './pages/ForInterview'
+import StudentPortfolio from './pages/StudentPortfolio'
+import * as ROUTES from './constants/routes'
 
-export interface IAppProps{}
+export interface IAppProps {}
 
-const  App:React.FunctionComponent<IAppProps> =()=> {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={ROUTES.HOME} element={<Home/>}/>
-                <Route path={ROUTES.LOGIN} element={<Login/>}/>
-                <Route path={ROUTES.AVAILABLE_STUDENTS} element={<AvailableStudents/>}/>
-                <Route path={ROUTES.FOR_INTERVIEW} element={<ForInterview/>}/>
-                <Route path={ROUTES.STUDENT_PORTFOLIO} element={<StudentPortfolio/>}/>
-            </Routes>
-        </BrowserRouter>
-
-
-    );
+const App: React.FunctionComponent<IAppProps> = () => {
+  return (
+      <BrowserRouter>
+         <Routes>
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route
+               path={ROUTES.AVAILABLE_STUDENTS}
+               element={<AvailableStudents />}
+            />
+            <Route path={ROUTES.FOR_INTERVIEW} element={<ForInterview />} />
+            <Route
+               path={ROUTES.STUDENT_PORTFOLIO}
+               element={<StudentPortfolio />}
+            />
+         </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
