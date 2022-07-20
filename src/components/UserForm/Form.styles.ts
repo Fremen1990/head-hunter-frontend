072d4130172835a9ce37a4fb40c8cd2cc818a710
Mateusz <nowe7246@gmail.com>
2022-07-20 10:09:13 +0200

    Created form structure with css.

--- src/components/UserForm/Form.styles.ts
+++ src/components/UserForm/Form.styles.ts
@@ -1,3 +1,53 @@
 import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
-export const FormContainer = styled.div``
+export const FormContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   flex-wrap: wrap;
+   width: 50vw;
+   height: 50vh;
+`
+export const Form = styled.form`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   margin-top: 5px;
+   width: 90%;
+   padding: 10px;
+
+   & input {
+      width: 100%;
+      border: none;
+      padding: 5px;
+      color: #e1e1fe;
+      background-color: rgba(255, 255, 255, 0.15);
+      margin: 10px 0;
+
+      &:focus {
+         background-color: rgb(256, 256, 256);
+         color: #000;
+      }
+   }
+
+   & button {
+      color: white;
+      border: none;
+      font-size: 1rem;
+      padding: 5px 15px;
+      margin: 5px;
+      background-color: red;
+      cursor: pointer;
+   }
+`
+
+export const ForgetPasswordLink = styled(NavLink)`
+   color: white;
+   align-self: flex-end;
+   margin-bottom: 10px;
+   padding: 0 10px;
+   text-decoration: none;
+`
--- src/components/UserForm/Login.form.tsx
+++ src/components/UserForm/Login.form.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { ForgetPasswordLink, Form, FormContainer } from './Form.styles'
 
 export const LoginForm = () => {
    return (
-      <>
-         <h1>form</h1>
-      </>
+      <FormContainer>
+         <Form>
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Hasło" />
+            <ForgetPasswordLink to="#">Zapomniałeś hasła?</ForgetPasswordLink>
+            <button>Zaloguj się</button>
+         </Form>
+      </FormContainer>
    )
 }
