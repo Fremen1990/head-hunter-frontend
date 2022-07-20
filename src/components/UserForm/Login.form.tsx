4b6f69e14f81bfcc72cf586bcf557bf0f574b493
Mateusz <nowe7246@gmail.com>
2022-07-20 10:29:03 +0200

    Implement react-hook-form

--- src/components/UserForm/Login.form.tsx
+++ src/components/UserForm/Login.form.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import { ForgetPasswordLink, Form, FormContainer } from './Form.styles'
+import { useForm } from 'react-hook-form'
+
+type Login = {
+   email: string
+   password: string
+}
 
 export const LoginForm = () => {
+   const { handleSubmit, register } = useForm<Login>()
+
+   const onSubmit = (data: Login) => {
+      console.log(data)
+   }
+
    return (
       <FormContainer>
-         <Form>
-            <input type="email" placeholder="E-mail" />
-            <input type="password" placeholder="Hasło" />
+         <Form onSubmit={handleSubmit(onSubmit)}>
+            <input
+               type="email"
+               {...register('email', { required: 'To pole jest wymagane' })}
+               placeholder="E-mail"
+            />
+            <input
+               type="password"
+               {...register('password', { required: 'To pole jest wymagane' })}
+               placeholder="Hasło"
+            />
             <ForgetPasswordLink to="#">Zapomniałeś hasła?</ForgetPasswordLink>
             <button>Zaloguj się</button>
          </Form>
