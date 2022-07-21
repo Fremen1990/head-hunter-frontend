33c5eb34663d5eac8042d3a02438f0e7f9554448
Maciek <maciej.stepien1994@gmail.com>
2022-07-21 16:23:14 +0200

    Update description file

--- src/constants/description/description.ts
+++ src/constants/description/description.ts
@@ -1,5 +1,24 @@
-export const Description = {
+export const description = {
    inputsFields: {
       placeholder: 'name',
+   },
+   userInterview: {
+      userReservation: 'rezerwacja do:',
+      courseEvaluation: 'ocena przejścia kursu',
+      activityAssessment: 'ocena aktywności i zaangażowania na kursie',
+      codeEvaluation: 'ocena kodu w projekcie własnym',
+      scrumAssessment: 'ocena pracy w zespole Scrum',
+      preferWork: 'preferowane miejscie pracy',
+      targetPlace: 'docelowe miasto, gdzie kandydat chce pracować',
+      contractType: 'oczekiwany typ kontraktu',
+      targetSalary: 'oczekiwanie miesięcczne wynagrodzenie netto',
+      practice: 'Zgoda na odbycie bezpłatnych praktyk/stażu na początek',
+      experience: 'Komercyjne dościadcznie w programowaniu',
+   },
+   buttons: {
+      showCv: 'pokaż cv',
+      disinterest: 'brak zainteresowania',
+      hired: 'zatrudniony',
+      bookCall: 'zarezerwuj rozmowe',
    },
 }
