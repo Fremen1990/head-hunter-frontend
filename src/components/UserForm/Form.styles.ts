3612eb857800f3c0199a41e63bd051fbac448470
Mateusz <nowe7246@gmail.com>
2022-07-21 22:46:46 +0200

    Change styles at form errors in validation.

--- src/components/UserForm/Form.styles.ts
+++ src/components/UserForm/Form.styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
+interface Props {
+   err: {} | undefined
+}
+
 export const FormContainer = styled.div`
    display: flex;
    flex-direction: column;
@@ -18,21 +22,6 @@ export const Form = styled.form`
    width: 350px;
    padding: 10px;
 
-   & input {
-      width: 350px;
-      border: none;
-      padding: 5px;
-      color: #f7f7f7;
-      background-color: #292a2b;
-      height: 40px;
-      margin: 10px 0;
-
-      &:focus {
-         background-color: rgb(256, 256, 256);
-         color: #000;
-      }
-   }
-
    & p {
       color: white;
       align-self: flex-start;
@@ -44,9 +33,25 @@ export const Form = styled.form`
       font-size: 1rem;
       padding: 5px 15px;
       margin: 5px;
-      background-color: red;
+      background-color: #e02735;
       cursor: pointer;
       align-self: flex-end;
+   }
+`
+
+export const Input = styled.input<Props>`
+   width: 350px;
+   ${(props) => props.theme.main}
+   border: 1px solid ${({ err }) => (err ? 'red' : 'transparent')};
+   padding: 5px;
+   color: #f7f7f7;
+   background-color: #292a2b;
+   height: 40px;
+   margin: 10px 0;
+
+   &:focus {
+      background-color: rgb(256, 256, 256);
+      color: #000;
    }
 `
 
@@ -56,4 +61,35 @@ export const ForgetPasswordLink = styled(NavLink)`
    margin-bottom: 10px;
    padding: 0 10px;
    text-decoration: none;
+`
+
+export const InputWrap = styled.div`
+   position: relative;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   user-select: none;
+
+   & div {
+      position: absolute;
+      width: 200px;
+      height: 35px;
+      left: -220px;
+      top: 13px;
+      font-size: 12px;
+      padding: 8px 5px;
+      background: #e02735;
+      color: #fff;
+      border-radius: 6px;
+
+      &:after {
+         content: '';
+         border-left: 15px solid #e02735;
+         border-top: 15px solid transparent;
+         border-bottom: 15px solid transparent;
+         position: absolute;
+         right: -14px;
+         top: 2.5px;
+      }
+   }
 `
--- src/components/UserForm/Login.form.tsx
+++ src/components/UserForm/Login.form.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { ForgetPasswordLink, Form, FormContainer } from './Form.styles'
+import {
+   ForgetPasswordLink,
+   Form,
+   FormContainer,
+   Input,
+   InputWrap,
+} from './Form.styles'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { emailValidate } from '../../constants/validation'
@@ -20,32 +26,37 @@ export const LoginForm = () => {
 
    const onSubmit = (data: Login) => {
       if (data.email && data.password) {
-         navigate('/')
+         navigate('/user')
       }
    }
 
    return (
       <FormContainer>
          <Form onSubmit={handleSubmit(onSubmit)}>
-            <input
-               type="email"
-               {...register('email', {
-                  required: 'Email jest wymagany',
-                  pattern: {
-                     value: emailValidate,
-                     message: 'Podaj swój adres email',
-                  },
-               })}
-               placeholder="E-mail"
-            />
-            <p>{errors.email?.message}</p>
-
-            <input
-               type="password"
-               {...register('password', { required: 'Hasło jest wymagane' })}
-               placeholder="Hasło"
-            />
-            <p>{errors.password?.message}</p>
+            <InputWrap>
+               <Input
+                  err={errors.email}
+                  type="email"
+                  {...register('email', {
+                     required: 'Email jest wymagany',
+                     pattern: {
+                        value: emailValidate,
+                        message: 'Podaj swój adres email',
+                     },
+                  })}
+                  placeholder="E-mail"
+               />
+               {errors.email && <div>{errors.email.message}</div>}
+            </InputWrap>
+            <InputWrap>
+               <Input
+                  err={errors.password}
+                  type="password"
+                  {...register('password', { required: 'Hasło jest wymagane' })}
+                  placeholder="Hasło"
+               />
+               {errors.password && <div>{errors.password.message}</div>}
+            </InputWrap>
             <ForgetPasswordLink to="#">Zapomniałeś hasła?</ForgetPasswordLink>
             <button>Zaloguj się</button>
          </Form>
