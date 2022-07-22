251dd9550411c0436fa465f3d9ae82335d4720be
albert_rogoz <105876587+RAlbert5477@users.noreply.github.com>
2022-07-23 00:07:38 +0200

    Updated mobile view (#20)
    * Added mobile view
    Feature/42.login page
    
    * Updated mobile avatar view
    Feature/43.user-page

--- src/constants/Layout/AccountContainer.styles.ts
+++ src/constants/Layout/AccountContainer.styles.ts
@@ -1,19 +1,24 @@
 import styled from 'styled-components'
 import { appColors } from './app.colors'
+import { device } from '../mediaQueries'
 
 export const AccountContainer = styled.div`
    display: flex;
    flex-direction: column;
    align-items: center;
-   width: 60%;
+   width: 90%;
    margin: 0 auto;
    border: 1px solid ${appColors.borderColor};
    background-color: ${appColors.pageContainerColor};
    color: ${appColors.fontColor};
    min-height: 89vh;
 
-   & .container-box {
-      width: 50%;
+   ${device.desktop} {
+      width: 60%;
+   }
+
+   & > .container-box {
+      width: 90%;
       border: 1px solid ${appColors.borderColor};
       box-shadow: 0 0 5px rgba(0, 0, 0, 1);
       min-height: 50px;
@@ -23,10 +28,19 @@ export const AccountContainer = styled.div`
       align-items: center;
       flex-direction: column;
    }
+   ${device.desktop} {
+      width: 60%;
+   }
 `
 export const AccountAvatar = styled.img`
-   max-width: 200px;
-   max-height: 200px;
+   max-width: 140px;
+   max-height: 140px;
    border-radius: 50%;
    margin: 40px 0;
+   box-shadow: 0 0 5px rgba(0, 0, 0, 1);
+
+   ${device.desktop} {
+      max-width: 190px;
+      max-height: 190px;
+   }
 `
