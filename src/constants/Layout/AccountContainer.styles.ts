3e3d3425f460fea5596de4c7989c437b541fd1cf
RAlbert5477 <rogoz.albert2003@gmail.com>
2022-07-22 21:59:37 +0200

    Fixed the conflict Feature/43.user-page

--- src/constants/Layout/AccountContainer.styles.ts
+++ src/constants/Layout/AccountContainer.styles.ts
@@ -1,24 +1,19 @@
 import styled from 'styled-components'
 import { appColors } from './app.colors'
-import { device } from '../mediaQueries'
 
 export const AccountContainer = styled.div`
    display: flex;
    flex-direction: column;
    align-items: center;
-   width: 80%;
+   width: 60%;
    margin: 0 auto;
    border: 1px solid ${appColors.borderColor};
    background-color: ${appColors.pageContainerColor};
    color: ${appColors.fontColor};
    min-height: 89vh;
 
-   ${device.desktop} {
-      width: 60%;
-   }
-
-   & > .container-box {
-      width: 90%;
+   & .container-box {
+      width: 50%;
       border: 1px solid ${appColors.borderColor};
       box-shadow: 0 0 5px rgba(0, 0, 0, 1);
       min-height: 50px;
@@ -27,18 +22,11 @@ export const AccountContainer = styled.div`
       justify-content: center;
       align-items: center;
       flex-direction: column;
-      font-size: 0.9rem;
-      text-align: center;
-
-      ${device.tablet} {
-         width: 60%;
-      }
    }
 `
 export const AccountAvatar = styled.img`
-   width: 200px;
-   height: 200px;
+   max-width: 200px;
+   max-height: 200px;
    border-radius: 50%;
    margin: 40px 0;
-   box-shadow: 0 0 5px rgba(0, 0, 0, 1);
 `
