40e36ede0b5d5891fc5e75be270fbeb527cb0a35
RAlbert5477 <rogoz.albert2003@gmail.com>
2022-07-22 21:59:37 +0200

    Fixed the conflict Feature/43.user-page

--- src/components/AccountProfile/AccountBox.tsx
+++ src/components/AccountProfile/AccountBox.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-interface Props {
-   accountName: string
-}
-
-export const AccountBox = (props: Props) => (
+export const AccountBox = () => (
    <div className="container-box">
-      <h2>Witaj, {props.accountName}</h2>
-      <p>Dzisaj jest {new Date().toLocaleDateString()}</p>
+      <h2>Witaj, [userName]</h2>
+      <p>Dzisaj jest [currData]</p>
    </div>
 )
