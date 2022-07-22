b83ff9f3f4e095029a95166e321b162c61847bc9
RAlbert5477 <rogoz.albert2003@gmail.com>
2022-07-22 21:59:37 +0200

    Fixed the conflict Feature/43.user-page

--- src/components/AccountProfile/User/User.tsx
+++ src/components/AccountProfile/User/User.tsx
@@ -13,7 +13,7 @@ export const User = () => {
             src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png"
             alt="avatar profilu użytkownika"
          />
-         <AccountBox accountName={'Jan Kowalski'} />
+         <AccountBox />
          <div className="container-box">
             <h3>Status</h3>
             <p>[statusInfo]</p>
