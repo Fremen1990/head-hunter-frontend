18660447c04da8d799476f127a4a1284eb856575
albert_rogoz <105876587+RAlbert5477@users.noreply.github.com>
2022-07-22 16:30:40 +0200

    Feature/43.user page (#17)
    * Added User component and styled UserContainer
    Feature/42.login page
    
    * Added User component and styled UserContainer
    Feature/42.login page
    
    * File names changed and style updated
    Feature/43.user-page
    
    * Added AccountBox component
    Feature/43.user-page
    
    * Added AccountBox component
    Feature/43.user-page
    
    * Added User component and styled UserContainer
    Feature/42.login page
    
    * Removed old files
    Feature/43.user-page
    
    * Removed old files
    Feature/43.user-page
    
    * Updated User.page.tsx view
    Feature/43.user-page

---
+++ src/components/AccountProfile/AccountBox.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const AccountBox = () => (
+   <div className="container-box">
+      <h2>Witaj, [userName]</h2>
+      <p>Dzisaj jest [currData]</p>
+   </div>
+)
