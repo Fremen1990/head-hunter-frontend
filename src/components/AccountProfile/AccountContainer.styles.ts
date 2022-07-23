eae817ea55a08ce13c605ccdaf63dccf7610afc9
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-23 19:00:38 +0200

    Move style to component folder. Edit: box shadow Add font size

--- src/components/AccountProfile/AccountContainer.styles.ts
+++ src/components/AccountProfile/AccountContainer.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import { appColors } from './app.colors'
-import { device } from '../mediaQueries'
+import { appColors } from '../../constants/Layout/app.colors'
+import { device } from '../../constants/mediaQueries'
 
 export const AccountContainer = styled.div`
    display: flex;
@@ -18,16 +18,22 @@ export const AccountContainer = styled.div`
    }
 
    & > .container-box {
-      width: 90%;
-      border: 1px solid ${appColors.borderColor};
-      box-shadow: 0 0 5px rgba(0, 0, 0, 1);
-      min-height: 50px;
-      margin-bottom: 50px;
       display: flex;
       justify-content: center;
       align-items: center;
       flex-direction: column;
+      width: 90%;
+      border: 1px solid ${appColors.borderColor};
+      box-shadow: 0 4px 15px rgba(0, 0, 0, 0.35);
+      min-height: 50px;
+      margin-bottom: 50px;
+      background-color: transparent;
+
+      & * {
+         font-size: 1.8rem;
+      }
    }
+
    ${device.desktop} {
       width: 60%;
    }
@@ -37,7 +43,7 @@ export const AccountAvatar = styled.img`
    max-height: 140px;
    border-radius: 50%;
    margin: 40px 0;
-   box-shadow: 0 0 5px rgba(0, 0, 0, 1);
+   box-shadow: 0 0 15px rgba(0, 0, 0, 0.35);
 
    ${device.desktop} {
       max-width: 190px;
