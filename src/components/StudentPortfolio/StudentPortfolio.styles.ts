a9baf6cc1cdb80a5b2ac8f9d37a2d0649b503290
Mateusz <nowe7246@gmail.com>
2022-07-24 09:45:54 +0200

    Created and styled aside section.

--- src/components/StudentPortfolio/StudentPortfolio.styles.ts
+++ src/components/StudentPortfolio/StudentPortfolio.styles.ts
@@ -1,6 +1,114 @@
+import { IoMdMail } from 'react-icons/io'
+import { FaPhoneAlt } from 'react-icons/fa'
+import { AiFillGithub } from 'react-icons/ai'
 import styled from 'styled-components'
+import { device } from '../../constants/mediaQueries'
 
 export const UserPortfolioContainer = styled.div`
    display: flex;
    align-items: flex-start;
+`
+export const AsideSection = styled.aside`
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   align-items: center;
+   flex-wrap: wrap;
+   width: 100px;
+
+   background-color: #292a2b;
+
+   ${device.desktop} {
+      width: 255px;
+   }
+
+   & div {
+      width: 80%;
+      background-color: #292a2b;
+   }
+
+   & p {
+      background-color: #292a2b;
+   }
+
+   & a {
+      text-decoration: none;
+      background-color: #292a2b;
+      color: #0b8bd4;
+   }
+
+   & img {
+      max-width: 80px;
+      max-height: 80px;
+      border-radius: 50%;
+      margin: 20px 0;
+
+      ${device.desktop} {
+         width: 150px;
+         height: 150px;
+      }
+   }
+
+   & Button {
+      width: 80%;
+   }
+`
+export const RowContainer = styled.div`
+   display: flex;
+   flex-direction: row;
+   justify-content: flex-start;
+   flex-wrap: wrap;
+   margin: 5px 0;
+   align-items: center;
+
+   & p {
+      margin-left: 10px;
+   }
+`
+
+export const GitHubContainer = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+
+   & a {
+      margin-left: 10px;
+   }
+`
+
+export const DescriptionContainer = styled.div`
+   margin: 10px 0;
+   width: 100%;
+
+   & div {
+      width: 100%;
+   }
+
+   & p {
+      align-self: flex-start;
+      color: #4d4d4d;
+   }
+`
+
+export const MainSection = styled.main`
+   background-color: #222324;
+   padding: 0 10px;
+   width: 250px;
+
+   ${device.desktop} {
+      width: 1176px;
+   }
+`
+
+export const GithubIcon = styled(AiFillGithub)`
+   color: #0b8bd4;
+   background-color: #292a2b;
+`
+export const MailIcon = styled(IoMdMail)`
+   color: #4d4d4d;
+   background-color: #292a2b;
+`
+export const PhoneIcon = styled(FaPhoneAlt)`
+   color: #4d4d4d;
+   background-color: #292a2b;
 `
--- src/components/StudentPortfolio/StudentPortfolio.tsx
+++ src/components/StudentPortfolio/StudentPortfolio.tsx
@@ -1,6 +1,49 @@
 import React from 'react'
-import { UserPortfolioContainer } from './StudentPortfolio.styles'
+import { Button } from '../commons/Button/Button'
+import {
+   AsideSection,
+   DescriptionContainer,
+   GitHubContainer,
+   GithubIcon,
+   MailIcon,
+   MainSection,
+   PhoneIcon,
+   RowContainer,
+   UserPortfolioContainer,
+} from './StudentPortfolio.styles'
+
+const loremText =
+   'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt\n' +
+   '                        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo'
 
 export const StudentPortfolio = () => {
-   return <UserPortfolioContainer></UserPortfolioContainer>
+   return (
+      <UserPortfolioContainer>
+         <AsideSection>
+            <img
+               src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png"
+               alt="avatar profilu użytkownika"
+            />
+            <p>Jan Kowalski</p>
+            <GitHubContainer>
+               <GithubIcon />
+               <a href="https://github.com/">jankowalski</a>
+            </GitHubContainer>
+            <RowContainer>
+               {' '}
+               <PhoneIcon /> <p>+48 393 393 393</p>
+            </RowContainer>
+            <RowContainer>
+               <MailIcon /> <p>test@przyklad.com</p>
+            </RowContainer>
+            <DescriptionContainer>
+               <p>O mnie</p>
+               <div>{loremText}</div>
+            </DescriptionContainer>
+            <Button buttonTitle="Brak zaintersowania" />
+            <Button buttonTitle="Zatrudniony" />
+         </AsideSection>
+         <MainSection />
+      </UserPortfolioContainer>
+   )
 }
