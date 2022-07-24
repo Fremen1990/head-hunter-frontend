cca693f0629eb8b699928c2a4f752c86a191391f
Mateusz <nowe7246@gmail.com>
2022-07-24 09:55:42 +0200

    Created and styled main section.

--- src/components/StudentPortfolio/StudentPortfolio.styles.ts
+++ src/components/StudentPortfolio/StudentPortfolio.styles.ts
@@ -1,6 +1,7 @@
 import { IoMdMail } from 'react-icons/io'
 import { FaPhoneAlt } from 'react-icons/fa'
-import { AiFillGithub } from 'react-icons/ai'
+import { BsPaperclip } from 'react-icons/bs'
+import { AiFillGithub, AiOutlineStar } from 'react-icons/ai'
 import styled from 'styled-components'
 import { device } from '../../constants/mediaQueries'
 
@@ -98,6 +99,74 @@ export const MainSection = styled.main`
    ${device.desktop} {
       width: 1176px;
    }
+
+   & a {
+      text-decoration: none;
+      background-color: #292a2b;
+      color: #0b8bd4;
+   }
+`
+
+export const RatingContainer = styled.div`
+   display: flex;
+   background-color: #292a2b;
+
+   & div {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      padding: 10px;
+   }
+   & div.star-container {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+   }
+   & div .rating-container {
+      display: flex;
+      flex-direction: row;
+   }
+   & span {
+      display: flex;
+   }
+   & h2 {
+      color: #cfcfcf;
+      font-size: 16px;
+   }
+`
+export const SubtitleSection = styled.div`
+   background-color: #292a2b;
+   padding: 10px;
+   font-size: 20px;
+   font-weight: bold;
+   color: #f7f7f7;
+`
+
+export const ExpectationContainer = styled.div`
+   display: flex;
+   gap: 5px;
+   background-color: #292a2b;
+
+   & div.container {
+      padding: 10px;
+   }
+
+   & h2 {
+      font-size: 14px;
+      color: #cfcfcf;
+   }
+
+   & p {
+   }
+`
+
+export const LinksSection = styled.div`
+   padding: 10px;
+   color: #0b8bd4; ;
+`
+
+export const DescriptionSection = styled.div`
+   padding: 10px;
 `
 
 export const GithubIcon = styled(AiFillGithub)`
@@ -112,3 +181,6 @@ export const PhoneIcon = styled(FaPhoneAlt)`
    color: #4d4d4d;
    background-color: #292a2b;
 `
+
+export const StarIcon = styled(AiOutlineStar)``
+export const ClipIcon = styled(BsPaperclip)``
--- src/components/StudentPortfolio/StudentPortfolio.tsx
+++ src/components/StudentPortfolio/StudentPortfolio.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Button } from '../commons/Button/Button'
 import {
    AsideSection,
+   ClipIcon,
    DescriptionContainer,
+   DescriptionSection,
+   ExpectationContainer,
    GitHubContainer,
    GithubIcon,
+   LinksSection,
    MailIcon,
    MainSection,
    PhoneIcon,
+   RatingContainer,
    RowContainer,
+   SubtitleSection,
    UserPortfolioContainer,
 } from './StudentPortfolio.styles'
 
@@ -30,7 +36,6 @@ export const StudentPortfolio = () => {
                <a href="https://github.com/">jankowalski</a>
             </GitHubContainer>
             <RowContainer>
-               {' '}
                <PhoneIcon /> <p>+48 393 393 393</p>
             </RowContainer>
             <RowContainer>
@@ -43,7 +48,86 @@ export const StudentPortfolio = () => {
             <Button buttonTitle="Brak zaintersowania" />
             <Button buttonTitle="Zatrudniony" />
          </AsideSection>
-         <MainSection />
+         <MainSection>
+            <SubtitleSection>Oceny</SubtitleSection>
+            <RatingContainer>
+               <div>Sekcja Ocen</div>
+            </RatingContainer>
+            <SubtitleSection>
+               Oczekiwanie w stosunku do zatrudnienia
+            </SubtitleSection>
+            <ExpectationContainer>
+               <div className="container">
+                  <h2>Preferowane miejsce pracy</h2>
+                  <p>Biuro</p>
+               </div>
+               <div className="container">
+                  <h2>Docelowe miasto, gdzie chce pracować kandydat</h2>
+                  <p>Biuro</p>
+               </div>
+               <div className="container">
+                  <h2>Oczekiwany typ kontraktu</h2>
+                  <p>Biuro</p>
+               </div>
+               <div className="container">
+                  <h2>Oczekiwane wynagrodzenie miesięczne netto</h2>
+                  <p>Biuro</p>
+               </div>
+               <div className="container">
+                  <h2>
+                     Zgoda na odbycie bezpłatnych praktyk/stażu na początek
+                  </h2>
+                  <p>Biuro</p>
+               </div>
+               <div className="container">
+                  <h2>Komercyjne doświadczenie w programowaniu</h2>
+                  <p>Biuro</p>
+               </div>
+            </ExpectationContainer>
+            <SubtitleSection>Edukacja</SubtitleSection>
+            <DescriptionSection>
+               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+               nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+               erat, sed diam voluptua. At vero eos et accusam et justo duo
+               dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+               sanctus est Lorem ipsum dolor sit amet.
+            </DescriptionSection>
+            <SubtitleSection>Kursy</SubtitleSection>
+            <DescriptionSection>
+               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+               nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+               erat, sed diam voluptua. At vero eos et accusam et justo duo
+               dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+               sanctus est Lorem ipsum dolor sit amet.
+            </DescriptionSection>
+            <SubtitleSection>Doświadczenie zawodowe</SubtitleSection>
+            <DescriptionSection>
+               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+               nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+               erat, sed diam voluptua. At vero eos et accusam et justo duo
+               dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+               sanctus est Lorem ipsum dolor sit amet.
+            </DescriptionSection>
+            <SubtitleSection>Portfolio</SubtitleSection>
+            <LinksSection>
+               <ClipIcon />
+               <a href="https://github.com/">https://github.com/</a>
+            </LinksSection>
+            <SubtitleSection> Projekt w zespole Scrumowym</SubtitleSection>
+            <LinksSection>
+               <ClipIcon />
+               <a href="https://github.com/">https://github.com/</a>
+            </LinksSection>
+            <LinksSection>
+               <ClipIcon />
+               <a href="https://github.com/">https://github.com/</a>
+            </LinksSection>
+            <SubtitleSection>Projekt na zaliczenie</SubtitleSection>
+            <LinksSection>
+               <ClipIcon />
+               <a href="https://github.com/">https://github.com/</a>
+            </LinksSection>
+         </MainSection>
       </UserPortfolioContainer>
    )
 }
