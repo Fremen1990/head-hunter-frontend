3cf19eab93f2771c67160b2eeabd0955cd98ce29
RAlbert5477 <rogoz.albert2003@gmail.com>
2022-07-25 20:13:20 +0200

    Added Admin component

--- src/pages/Admin.page.tsx
+++ src/pages/Admin.page.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { PageContainer } from '../constants/Layout/Container.styles'
 import { Header } from '../components/Header/Header'
+import { Admin } from '../components/AccountProfile/Admin/Admin'
 
 export const AdminPage = () => {
    return (
       <>
          <Header />
          <PageContainer>
-            <h1>Admin Page</h1>
-            <p>
-               For example how to use page container with header component :)
-            </p>
+            <Admin />
          </PageContainer>
       </>
    )
