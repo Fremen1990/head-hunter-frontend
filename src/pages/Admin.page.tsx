6912ba13fc3f2987b39b04b1de7add76f4311e9e
DevThomas <fremen1990@gmail.com>
2022-07-25 03:45:09 +0200

    Boilerplate with routes for all mockup components

--- src/pages/Admin.page.tsx
+++ src/pages/Admin.page.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PageContainer } from '../constants/Layout/Container.styles'
 import { Header } from '../components/Header/Header'
+import { getStudents, importStudentsCall, uploadFileCall } from '../apiCalls'
+import ReactJson from 'react-json-view'
 
 export const AdminPage = () => {
+   const [uploadFile, setUploadFile] = useState('')
+   const [studentsJson, setStudentsJson] = useState([])
+   const [getStudentsJson, setGetStudentsJson] = useState([])
+   const [importStatus, setImportStatus] = useState([])
+   const uploadFileHandler = (e) => {
+      setUploadFile(e.target.files[0])
+   }
+
+   const sendFileHandler = async () => {
+      const data = await new FormData()
+      data.append('usersImport', uploadFile)
+
+      console.log(data)
+      const response = await uploadFileCall(data)
+      setStudentsJson(response)
+      console.log(response)
+   }
+
+   const importStudentsHandler = async () => {
+      const reponse = await importStudentsCall(studentsJson)
+      setStudentsJson([])
+      setImportStatus(reponse)
+   }
+
+   const getStudentsHandler = async () => {
+      console.log('GET STUDENTS')
+
+      const response = await getStudents()
+      setGetStudentsJson(response)
+   }
+
    return (
       <>
          <Header />
@@ -11,6 +44,22 @@ export const AdminPage = () => {
             <p>
                For example how to use page container with header component :)
             </p>
+            <button onClick={() => sendFileHandler()}>
+               Upload Students file{' '}
+            </button>
+            <button onClick={importStudentsHandler}>
+               Import students to database{' '}
+            </button>
+            <button onClick={getStudentsHandler}>Get Students</button>
+
+            <label htmlFor="">Brose students file to import</label>
+            <input type="file" name="file" onChange={uploadFileHandler} />
+
+            {studentsJson && <ReactJson src={studentsJson} />}
+
+            <ReactJson src={importStatus} />
+
+            <ReactJson src={getStudentsJson} />
          </PageContainer>
       </>
    )
