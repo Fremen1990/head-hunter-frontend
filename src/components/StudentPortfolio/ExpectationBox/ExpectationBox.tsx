f286361aeadf0cee5c566a88cbdac3cbd495160d
Mateusz <nowe7246@gmail.com>
2022-07-25 13:27:44 +0200

    Created ExpectationBox component.

---
+++ src/components/StudentPortfolio/ExpectationBox/ExpectationBox.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { SubtitlesSection } from '../SubtitlesSection/SubtitlesSection'
+import { MoreInfoBox } from '../../recruiters/OneUser/OneUser.styles'
+import { InterViewMeBox } from '../../recruiters/OneUser/InterViewMeBox/InterViewMeBox'
+import { description } from '../../../constants/description/description'
+
+export const ExpectationBox = () => {
+   const {
+      preferWork,
+      targetPlace,
+      contractType,
+      targetSalary,
+      practice,
+      experience,
+   } = description.userInterview
+
+   return (
+      <>
+         <SubtitlesSection text={'Oczekiwanie w stosunku do zatrudnienia'} />
+         <MoreInfoBox>
+            <InterViewMeBox
+               ratingBox={false}
+               boxTitle={preferWork}
+               point={'Biuro'}
+            />
+
+            <InterViewMeBox
+               ratingBox={false}
+               boxTitle={targetPlace}
+               point={'WARSZAWA'}
+            />
+            <InterViewMeBox
+               ratingBox={false}
+               boxTitle={contractType}
+               point={'Umowa o prace'}
+            />
+            <InterViewMeBox
+               ratingBox={false}
+               boxTitle={targetSalary}
+               point={'8000 zł'}
+            />
+            <InterViewMeBox
+               ratingBox={false}
+               boxTitle={practice}
+               point={'tak'}
+            />
+            <InterViewMeBox
+               ratingBox={false}
+               boxTitle={experience}
+               point={'6 mies'}
+            />
+         </MoreInfoBox>
+      </>
+   )
+}
