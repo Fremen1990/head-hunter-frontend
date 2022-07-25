ff0712bc89002563de8e999ad4a384427ef31048
Mateusz <nowe7246@gmail.com>
2022-07-25 13:30:09 +0200

    Add props to LinksBox to fix shows SubtitlesSection to many times.

--- src/components/StudentPortfolio/LinksBox/LinksBox.tsx
+++ src/components/StudentPortfolio/LinksBox/LinksBox.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { SubtitlesSection } from '../SubtitlesSection/SubtitlesSection'
 import { LinksSection, ClipIcon } from './LinksBox.styles'
 
 interface Props {
-   link: string
+   text?: string
+   link?: string
 }
 
 export const LinksBox = (props: Props) => {
    return (
-      <LinksSection>
-         <ClipIcon />
-         <a href={`${props.link}`}>{props.link}</a>
-      </LinksSection>
+      <>
+         {props.text === undefined ? null : (
+            <SubtitlesSection text={props.text} />
+         )}
+         {props.link === undefined ? null : (
+            <LinksSection>
+               <ClipIcon />
+               <a href={`${props.link}`}>{props.link}</a>
+            </LinksSection>
+         )}
+      </>
    )
 }
