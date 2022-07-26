2e1586cb199ba257882b515ae21fb04c5f899930
mateuszslowinski <105711183+mateuszslowinski@users.noreply.github.com>
2022-07-26 21:52:24 +0200

    Feature/11.student portfolio (#26)
    * Feature/11.StudentPortfolio
    
    * Created and styled aside section.
    
    * Created and styled main section.
    
    * Created LinksBox and styled.
    
    * Created AsideUserBox and styled it.
    
    * Created SubtitlesSection component and styled it.
    
    * Created DescriptionBox and styled it.
    
    * Install react-star-ratings
    
    * Created RatingVewBox component.
    
    * Created RatingBox component.
    
    * Created ExpectationBox component.
    
    * Add props to LinksBox to fix shows SubtitlesSection to many times.
    
    * Updated structure StudentPortfolio and styles.
    
    * Updated LinksBox css.
    
    * Updated DescriptionBox css.
    
    * Add new constants colors.
    
    * Updated RatingViewBox css.
    
    * Added new constants description.
    
    * Updated AsideBox css and added new constant.
    
    * Updated ExpectationBo for new constant.
    
    * Updated RatingBox for new constant.
    
    * Updated StudentPortfolio css and added new constant.
    
    * Created ExpectationBox styles file.
    
    * Adjusted mobile view.
    
    * Added Link to back to main page.
    
    * Added new routes and used to show student cv page.
    
    Co-authored-by: Mateusz <nowe7246@gmail.com>

---
+++ src/components/StudentPortfolio/DescriptionBox/DescriptionBox.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const DescriptionSection = styled.div`
+   padding: 10px;
+   font-size: 1.6rem;
+`
---
+++ src/components/StudentPortfolio/SubtitlesSection/SubtitlesSection.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const SubtitleSection = styled.div`
+   background-color: #292a2b;
+   padding: 10px;
+   font-size: 20px;
+   font-weight: bold;
+   color: #f7f7f7;
+`
