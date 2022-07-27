afa5d3dbd1368e3936db5e43aaa743bcd67e44da
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-28 00:47:25 +0200

    Add test page and add filtering modal

--- src/pages/Test.page.tsx
+++ src/pages/Test.page.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from '../components/Header/Header'
 import { PageContainer } from '../constants/Layout/Container.styles'
+import { Filtering } from '../components/recruiters/Filtering/Filtering'
 
 export const TestPage = () => {
+   const [isOpen, setIsOpen] = useState(false)
    return (
       <>
          <Header />
          <PageContainer>
             <h1> TEST PAGE </h1>
+            <button onClick={() => setIsOpen(!isOpen)}>Filtering</button>
+            {isOpen && <Filtering hiddenModal={() => setIsOpen(!isOpen)} />}
          </PageContainer>
       </>
    )
