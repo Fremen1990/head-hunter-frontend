d3ad745cb49578dc2f5633e72703f30b0231c166
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-27 20:34:11 +0200

    Separate one element to component folder

--- src/components/recruiters/Filtering/FilterForm/FilterForm.styles.ts
+++ src/components/recruiters/Filtering/FilterForm/FilterForm.styles.ts
@@ -4,7 +4,6 @@ import { appColors } from '../../../../constants/Layout/app.colors'
 export const Form = styled.form`
   background-color: transparent;
 
-
   & h2 {
     font-size: 3rem
   }
@@ -23,51 +22,4 @@ export const Form = styled.form`
     font-size: 1.4rem;
     background-color: transparent;
   }
-
-`
-
-export const StarInput = styled.div`
-   background-color: transparent;
-   margin-right: 5px;
-
-   & label {
-      height: 30px;
-      width: 40px;
-      font-size: 2rem;
-      border-radius: 2px;
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      cursor: pointer;
-      transition: 0.2s;
-
-      &:hover {
-         background-color: ${appColors.buttonColor};
-
-         & svg {
-            color: ${appColors.fontColor};
-         }
-      }
-
-      & svg {
-         font-size: 2rem;
-         background-color: transparent;
-         margin-left: 5px;
-         color: ${appColors.buttonColor};
-         transition: 0.2s;
-      }
-   }
-
-   & input[type='checkbox'] {
-      display: none;
-   }
-
-   & input:checked + label {
-      background-color: ${appColors.buttonColor};
-   }
-
-   & input:checked + label > svg {
-      color: ${appColors.fontColor};
-   }
 `
