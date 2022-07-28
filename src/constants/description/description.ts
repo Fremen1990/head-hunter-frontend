79617e4e5eae08fb37fe9bb1fa9b410e7e5ba3cc
Maciek <maciej.stepien1994@gmail.com>
2022-07-28 12:47:40 +0200

    Update description text

--- src/constants/description/description.ts
+++ src/constants/description/description.ts
@@ -65,4 +65,16 @@ export const description = {
       monthsOfCommercialExpErrorMessage:
          'doświadczenie w programowaniu może mieć maksymalnie 11 znaków',
    },
+   filterModal: {
+      courseEvaluation: 'ocena przejścia kursu',
+      activityAssessment: 'ocena aktywności na kursie',
+      codeEvaluation: 'ocena kodu w pojekcie własnym',
+      jobEvaluation: 'ocena pracy w zespole SCRUM',
+      typeOfWork: 'preferowane miejsce pracy',
+      typeOfContract: 'preferowany typ kontraktu',
+      salary: 'Oczekiwane wynagrodzenie miesięczne netto',
+      internship: 'zgoda na odbycie bezpłatnych praktyk/stażu na początek',
+      experience:
+         'Ilośc miesięcy doświacznia komercyjnego kandydata w programowaniu',
+   },
 }
