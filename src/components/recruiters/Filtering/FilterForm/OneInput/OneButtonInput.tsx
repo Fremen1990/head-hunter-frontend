ac2e4ae42a485aebfd3729bd3753b2be33bbbfcc
Maciek <maciej.stepien1994@gmail.com>
2022-07-28 12:13:16 +0200

    Add button Input

---
+++ src/components/recruiters/Filtering/FilterForm/OneInput/OneButtonInput.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { ButtonInput } from './Input.styles'
+
+interface Props {
+   // @ts-ignore
+   onChange: ({ type: string, payload }) => void
+   name: string
+   value: string
+   inputName: string
+}
+
+export const OneButtonInput = (props: Props) => {
+   console.log(props.name, props.value)
+   return (
+      <ButtonInput>
+         <input
+            type="checkbox"
+            id={`${props.name}${props.value}`}
+            name={props.name}
+            value={props.value}
+            onChange={(e) =>
+               props.onChange({
+                  type: `${props.name.toUpperCase()}_UPDATE`,
+                  payload: e.target,
+               })
+            }
+         />
+         <label htmlFor={`${props.name}${props.value}`}>
+            {props.inputName}
+         </label>
+      </ButtonInput>
+   )
+}
