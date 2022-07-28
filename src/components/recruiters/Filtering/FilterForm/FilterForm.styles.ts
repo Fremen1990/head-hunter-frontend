8f96250c1900af19229095ddaf7fbf07170b5e30
Maciek <maciej.stepien1994@gmail.com>
2022-07-28 16:11:07 +0200

    Refactor style add outline color

--- src/components/recruiters/Filtering/FilterForm/FilterForm.styles.ts
+++ src/components/recruiters/Filtering/FilterForm/FilterForm.styles.ts
@@ -8,6 +8,10 @@ export const Form = styled.form`
     margin-right: 2px;
     font-size: 2rem
   }
+
+  & p:first-letter {
+    text-transform: uppercase;
+  }
 }
 
 & > div {
@@ -17,10 +21,43 @@ export const Form = styled.form`
   padding: 5px;
   background-color: transparent;
 
+  & div.internshipBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: transparent;
+
+    & label {
+      display: flex;
+      flex-direction: row-reverse;
+      align-items: center;
+      background-color: transparent;
+      font-size: 1.4rem;
+      padding: 3px;
+      cursor: pointer;
+
+      & input[type='radio' ] {
+        appearance: none;
+        background-color: transparent;
+        width: 1.15rem;
+        height: 1.15rem;
+        border: 0.15rem solid ${appColors.borderColor};
+        border-radius: 50%;
+        margin-right: 5px;
+        transition: 0.3s;
+
+        &:checked {
+          background-color: ${appColors.buttonColor};
+        }
+      }
+    }
+  }
+
   & p {
     width: 100%;
     color: ${appColors.fontColor};
     font-size: 1.4rem;
     background-color: transparent;
   }
+
 `
