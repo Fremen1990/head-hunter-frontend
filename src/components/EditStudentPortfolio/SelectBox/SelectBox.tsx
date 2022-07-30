5a14f20fa2d3371a1bf93c6f1be89dcaf29ccbda
Mateusz <nowe7246@gmail.com>
2022-07-30 20:02:54 +0200

    Created SelectBox.

---
+++ src/components/EditStudentPortfolio/SelectBox/SelectBox.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BoxContainer } from '../EditExpectationBox/EditExpectationBox.styles'
+
+interface Props {
+   title: string
+   options: { value: string; text: string }[]
+   method: (e: React.ChangeEvent<HTMLSelectElement>) => void
+}
+
+export const SelectBox = ({ title, options, method }: Props) => {
+   return (
+      <BoxContainer>
+         <p>{title}</p>
+         <select onChange={method}>
+            {options.map((option) => (
+               <option key={option.value} value={option.value}>
+                  {option.text}
+               </option>
+            ))}
+         </select>
+      </BoxContainer>
+   )
+}
