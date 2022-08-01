46755939a750c07b9c31935a063ac70adc81cf22
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-08-01 15:47:44 +0200

    Add export initial state for Reducer

--- src/components/commons/Button/Button.tsx
+++ src/components/commons/Button/Button.tsx
@@ -3,13 +3,8 @@ import { PrimaryButton } from './Button.styles'
 
 interface Props {
    buttonTitle: string
-   method?: () => void
 }
 
-export const Button = ({ method, buttonTitle, ...restProps }: Props) => {
-   return (
-      <PrimaryButton onClick={method} {...restProps}>
-         {buttonTitle}
-      </PrimaryButton>
-   )
+export const Button = (props: Props) => {
+   return <PrimaryButton>{props.buttonTitle}</PrimaryButton>
 }
