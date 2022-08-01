eade07c270a99884684813df72be7dfe31fb442c
DevThomas <fremen1990@gmail.com>
2022-08-01 20:48:47 +0200

    Header redux data test

--- src/components/Header/UserBox/UserBox.tsx
+++ src/components/Header/UserBox/UserBox.tsx
@@ -3,13 +3,21 @@ import { UserContainer } from './UserBox.styles'
 import { AiOutlineCaretDown } from 'react-icons/ai'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { description } from '../../../constants/description/description'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { userLogout } from '../../../features/user/userActions'
 
 export const UserBox = () => {
+   const { userInfo } = useSelector((state) => state.user)
+
    const [isOpen, setIsOpen] = useState<boolean>(false)
    const dispatch = useDispatch()
    const navigate = useNavigate()
+
+   // TODO To be rebuild from BE SIDE, send one array to avoid nesting
+   const gitHubUserName = userInfo ? userInfo?.userDetails : null
+   const gitPhoto = gitHubUserName ? gitHubUserName[0].githubUserName : null
+   const firstName = gitHubUserName ? gitHubUserName[0].firstName : null
+   const lastName = gitHubUserName ? gitHubUserName[0].lastName : null
 
    const handleLogout = () => {
       dispatch(userLogout())
@@ -20,9 +28,23 @@ export const UserBox = () => {
       <UserContainer isOpen={isOpen}>
          <div>
             <NavLink to={'/user'}>
-               <div className={'avatar'}></div>
+               <div className={'avatar'}>
+                  {gitHubUserName && (
+                     <img
+                        src={`https://avatars.githubusercontent.com/${gitPhoto}`}
+                        alt="gitHub avatar"
+                        style={{
+                           verticalAlign: 'middle',
+                           height: 35,
+                           width: 35,
+                        }}
+                     />
+                  )}
+               </div>
             </NavLink>
-            <p>User Name </p>
+            <p>
+               {firstName} {lastName}
+            </p>
             <AiOutlineCaretDown onClick={() => setIsOpen(!isOpen)} />
          </div>
          {isOpen && (
