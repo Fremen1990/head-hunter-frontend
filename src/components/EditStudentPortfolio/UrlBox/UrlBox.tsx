421628c66c6eb72907b2c5cafeeac91048710279
Mateusz <nowe7246@gmail.com>
2022-08-02 19:38:26 +0200

    Refactor code of UrlBox.

--- src/components/EditStudentPortfolio/UrlBox/UrlBox.tsx
+++ src/components/EditStudentPortfolio/UrlBox/UrlBox.tsx
@@ -15,20 +15,18 @@ export const UrlBox = ({
    inputMethod,
    btnMethod,
    btnText,
-}: Props) => {
-   return (
-      <label>
-         <SubtitlesSection text="Projekt na zaliczenie" />
-         {urlBoxArray.map((project: string, index: number) => (
-            <p key={index}>{project}</p>
-         ))}
-         <input
-            type="text"
-            name="projectUrls"
-            value={value}
-            onChange={inputMethod}
-         />
-         <button onClick={btnMethod}>{btnText}</button>
-      </label>
-   )
-}
+}: Props) => (
+   <label>
+      <SubtitlesSection text="Projekt na zaliczenie" />
+      {urlBoxArray.map((project: string) => (
+         <p key={project}>{project}</p>
+      ))}
+      <input
+         type="text"
+         name="projectUrls"
+         value={value}
+         onChange={inputMethod}
+      />
+      <button onClick={btnMethod}>{btnText}</button>
+   </label>
+)
