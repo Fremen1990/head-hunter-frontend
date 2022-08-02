288d32ba7ed04be589893b7fb9da315ed3ca4545
Maciej Stępień <83340294+MS151994@users.noreply.github.com>
2022-08-03 00:32:17 +0200

    Revert "Feature/10.filtering modal (#29)" (#38)
    This reverts commit a63ef9b0d6576fc3009c9838396556b809b74c30.

--- src/pages/Test.page.tsx
+++ src/pages/Test.page.tsx
@@ -1,17 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Header } from '../components/Header/Header'
 import { PageContainer } from '../constants/Layout/Container.styles'
-import { Filtering } from '../components/recruiters/Filtering/Filtering'
 
 export const TestPage = () => {
-   const [isOpen, setIsOpen] = useState(false)
    return (
       <>
          <Header />
          <PageContainer>
             <h1> TEST PAGE </h1>
-            <button onClick={() => setIsOpen(!isOpen)}>Filtering</button>
-            {isOpen && <Filtering hiddenModal={() => setIsOpen(!isOpen)} />}
          </PageContainer>
       </>
    )
