0224f02979cc8963fa61e289b62e3f7fbcf3b30a
DevThomas <fremen1990@gmail.com>
2022-08-03 02:48:29 +0200

    70. FE - Redux Toolkit - user update PATCH reducer

--- src/features/user/userActions.ts
+++ src/features/user/userActions.ts
@@ -58,3 +58,45 @@ export const userLogout = createAsyncThunk(
       }
    }
 )
+
+export const userUpdateProfile = createAsyncThunk(
+   'user/updateProfile',
+   async (formData: any, thunkAPI) => {
+      try {
+         const state = await thunkAPI.getState()
+         const response = await api.put(`/student/${state.user.id}`, formData)
+         const data = response.data
+         if (response.status === 200) {
+            return data
+         } else {
+            return thunkAPI.rejectWithValue(data.error)
+         }
+      } catch (e: any) {
+         console.log('Error', e.response.data)
+         return thunkAPI.rejectWithValue(e.response.data)
+      }
+   }
+)
+
+// dispatch(userUpdateProfile(userUpdateMock))
+//
+//
+// const userUpdateMock = {
+//   studentStatus: 'available',
+//   tel: '666-666-666',
+//   firstName: 'Tomasz UPDATED',
+//   lastName: 'UPDATEOWNIK',
+//   githubUserName: 'fremen1990',
+//   portfolioUrls: ['rv', 'rfev'],
+//   projectUrls: ['rv', 'rfev'],
+//   bio: 'My bio is aweeeesomeeeeeeeeee',
+//   expectedTypeOfWork: 'any',
+//   targetWorkCity: 're',
+//   expectedContractType: 'any',
+//   expectedSalary: '6000 USD',
+//   canTakeApprenticeship: 'yes',
+//   monthsOfCommercialExp: 5,
+//   education: '',
+//   workExperience: '',
+//   courses: '',
+// }
