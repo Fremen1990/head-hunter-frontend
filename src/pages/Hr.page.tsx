ff6da9caa05d142967396d8c62bf8bd9cf447625
Mateusz <nowe7246@gmail.com>
2022-08-04 23:12:09 +0200

    Create basic logic of hr page.

--- src/pages/Hr.page.tsx
+++ src/pages/Hr.page.tsx
@@ -3,12 +3,11 @@ import { PageContainer } from '../constants/Layout/Container.styles'
 import { Header } from '../components/Header/Header'
 import { NavigationBar } from '../components/recruiters/Navigation/Navigation'
 import { UserResultsContainer } from '../components/recruiters/UserResultsContainer/UserResultsContainer'
-import { SearchFilter } from '../components/Dashboard/SearchFilterBar/SearchFilterForm'
-import { getStudents } from '../apiCalls'
 import { FilterSection } from '../components/recruiters/FilterSection/FilterSection'
-import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { UserState } from '../features/user/userSlice'
 import { useAppSelector } from '../app/hooks'
+import { fetchHrCandidates } from '../features/hr/hrActions'
 
 // to remove later
 export interface studentsInterface {
@@ -41,18 +40,28 @@ export interface studentsInterface {
 }
 
 export const HrPage = () => {
-   // const [students, setStudents] = useState<studentsInterface[]>([])
+   const [students, setStudents] = useState<studentsInterface[]>([])
+   const [interviewStudents, setInterviewStudents] = useState<
+      studentsInterface[]
+   >([])
    const [toInterview, setToInterview] = useState<string>('Dotępni kursanci')
-
+   const dispatch = useDispatch()
    const { candidates } = useAppSelector((state: UserState) => state.hr)
 
-   console.log('candidates ', candidates)
-   // useEffect(() => {
-   //    ;(async () => {
-   //       const newStudents = await getStudents()
-   //       setStudents(newStudents)
-   //    })()
-   // }, [])
+   useEffect(() => {
+      setStudents(candidates)
+   }, [])
+
+   useEffect(() => {
+      ;(async () => {
+         // @ts-ignore
+         const newInterviewStudents = students.filter(
+            (user) => user.student.studentStatus === 'interview'
+         )
+         setInterviewStudents(newInterviewStudents)
+         dispatch(fetchHrCandidates())
+      })()
+   }, [toInterview])
 
    return (
       <>
@@ -64,10 +73,10 @@ export const HrPage = () => {
             {toInterview === 'Do rozmowy' ? (
                <UserResultsContainer
                   layout={'extended'}
-                  students={candidates}
+                  students={interviewStudents}
                />
             ) : (
-               <UserResultsContainer layout={'simple'} students={candidates} />
+               <UserResultsContainer layout={'simple'} students={students} />
             )}
          </PageContainer>
       </>
