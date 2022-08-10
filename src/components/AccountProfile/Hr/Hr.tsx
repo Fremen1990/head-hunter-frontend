5e54d48abe6b9136dd2edacb238774beaa40d807
RAlbert5477 <rogoz.albert2003@gmail.com>
2022-08-10 22:25:05 +0200

    Added style Feature/78.hr-page

--- src/components/AccountProfile/Hr/Hr.tsx
+++ src/components/AccountProfile/Hr/Hr.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
-import { AccountAvatar, AccountContainer } from '../AccountContainer.styles'
+import {
+   AccountAvatar,
+   AccountContainer,
+   UtilsMenuBox,
+   UtilsMenu,
+} from '../AccountContainer.styles'
 import { AccountBox } from '../AccountBox'
+import { IoPersonAdd, IoCreate } from 'react-icons/io5'
+import { NavLink } from 'react-router-dom'
 
 export const Hr = () => {
    return (
@@ -9,8 +16,19 @@ export const Hr = () => {
             src="https://www.clipartmax.com/png/middle/48-483031_github-logo-black-and-white-github-icon-vector.png"
             alt="avatar profilu użytkownika"
          />
-
          <AccountBox accountName={'HR'} />
+         <UtilsMenu>
+            <NavLink to="/hr">
+               <UtilsMenuBox>
+                  <IoPersonAdd />
+                  <p> Zarezerwuj rozmowę </p>
+               </UtilsMenuBox>
+            </NavLink>
+            <UtilsMenuBox>
+               <IoCreate />
+               <p> Zmień Hasło</p>
+            </UtilsMenuBox>
+         </UtilsMenu>
       </AccountContainer>
    )
 }
