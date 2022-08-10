f286383d530d18d32d04c7c376ef0d099d0d53f1
Mateusz <nowe7246@gmail.com>
2022-08-10 12:42:42 +0200

    Added styles and structure for CandidateCv.

--- src/components/CandidateCv/CandidateCv.tsx
+++ src/components/CandidateCv/CandidateCv.tsx
@@ -1,14 +1,43 @@
 import React from 'react'
-import { useAppSelector } from '../../app/hooks'
-import { Spinner } from '../commons/Spinner/Spinner'
+import {
+   AsideSection,
+   BackButton,
+   BackIcon,
+   CandidateCvContainer,
+   MainSection,
+} from './CandidateCv.styles'
+import { useNavigate } from 'react-router-dom'
+import { description } from '../../constants/description/description'
+import { AsideCandidateCvBox } from './AsideCandidateCvBox/AsideCandidateCvBox'
+import { DescriptionCandidateCvBox } from './DescriptionCandidateCvBox/DescriptionCandidateCvBox'
+import { RatingCandidateCvBox } from './RatingCandidateCvBox/RatingCandidateCvBox'
+import { ExpectationCandidateBox } from './ExpectationCandidateBox/ExpectationCandidateBox'
+import { LinksCandidateBox } from './LinksCadidateBox/LinksCandidateBox'
 
 export const CandidateCv = () => {
-   const { isFetching } = useAppSelector((state) => state.hr)
+   const navigate = useNavigate()
+
+   const { backFromModal } = description.studentPortfolio
 
    return (
-      <>
-         <h1>candiddate cv</h1>
-         {isFetching && <Spinner />}
-      </>
+      <CandidateCvContainer>
+         <BackButton onClick={() => navigate(-1)}>
+            <BackIcon />
+            {backFromModal}
+         </BackButton>
+         <AsideSection>
+            <AsideCandidateCvBox />
+         </AsideSection>
+         <MainSection>
+            <RatingCandidateCvBox></RatingCandidateCvBox>
+            <ExpectationCandidateBox></ExpectationCandidateBox>
+            <DescriptionCandidateCvBox></DescriptionCandidateCvBox>
+            <DescriptionCandidateCvBox></DescriptionCandidateCvBox>
+            <DescriptionCandidateCvBox></DescriptionCandidateCvBox>
+            <LinksCandidateBox></LinksCandidateBox>
+            <LinksCandidateBox></LinksCandidateBox>
+            <LinksCandidateBox></LinksCandidateBox>
+         </MainSection>
+      </CandidateCvContainer>
    )
 }
--- src/pages/CandidateCv.page.tsx
+++ src/pages/CandidateCv.page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { CandidateCv } from '../components/CandidateCv/CandidateCv'
 import { Header } from '../components/Header/Header'
 import styled from 'styled-components'
+import { api } from '../utils/axios'
+import { Spinner } from '../components/commons/Spinner/Spinner'
+import { useAppSelector } from '../app/hooks'
 
 export const CandidateCvPage = () => {
+   const { isFetching } = useAppSelector((state) => state.hr)
+
    const CandidateContainer = styled.div`
       display: flex;
       width: 100%;
@@ -15,6 +20,19 @@ export const CandidateCvPage = () => {
       max-width: 1450px;
       margin: 26px;
    `
+   useEffect(() => {
+      ;(async () => {
+         try {
+            const res = await api.get(
+               '/student/4fff930d-aea3-405c-99b4-f673f04e2c53'
+            )
+            const data = await res.data
+            console.log(data)
+         } catch (e) {
+            console.log((e as Error).message)
+         }
+      })()
+   }, [])
 
    return (
       <>
@@ -24,6 +42,7 @@ export const CandidateCvPage = () => {
                <CandidateCv />
             </CandidateCvContainer>
          </CandidateContainer>
+         {isFetching && <Spinner />}
       </>
    )
 }
