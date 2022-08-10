923a92b8038a7df953a6f918ebc6cd44937cff5c
Mateusz <nowe7246@gmail.com>
2022-08-10 12:03:17 +0200

    Created DescriptionCandidateCvBox.

---
+++ src/components/CandidateCv/DescriptionCandidateCvBox/DescriptionCandidateCvBox.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SubtitlesCandidateSection } from '../SubtitlesCandidateSection/SubtitlesCandidateSection'
+import { DescriptionSection } from './DescriptionCandidateCvBox.styles'
+
+export const DescriptionCandidateCvBox = () => (
+   <>
+      <SubtitlesCandidateSection />
+      <DescriptionSection>jakis opsi</DescriptionSection>
+   </>
+)
