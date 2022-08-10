55791d41fd0fc372a66e67efc3de822c637b46a7
Mateusz <nowe7246@gmail.com>
2022-08-10 12:04:08 +0200

    Created SubtitlesCandidateSection.

---
+++ src/components/CandidateCv/SubtitlesCandidateSection/SubtitlesCandidateSection.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { SubtitleSection } from './SubtitlesCandidateSection.styles'
+
+export const SubtitlesCandidateSection = () => (
+   <SubtitleSection>jakis opis</SubtitleSection>
+)
