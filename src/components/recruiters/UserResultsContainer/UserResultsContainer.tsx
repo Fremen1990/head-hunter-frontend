25f844a4af7e722373a7ab8241f58ad683f9ac86
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-08-10 22:41:29 +0200

    Add pagination to component

--- src/components/recruiters/UserResultsContainer/UserResultsContainer.tsx
+++ src/components/recruiters/UserResultsContainer/UserResultsContainer.tsx
@@ -1,42 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { OneUser } from '../OneUser/OneUser'
 import { studentsInterface } from 'src/pages/Hr.page'
 // eslint-disable-next-line no-unused-vars
 // eslint-disable-next-line no-unused-vars
 import { ResultsContainer } from './UserResultsContainer.styles'
+import ReactPaginate from 'react-paginate'
+import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md'
 
 interface Props {
    students: studentsInterface[]
    layout: string
    refreshStudents: () => void
-   searchingValue: string
 }
 
 export const UserResultsContainer = ({
    layout,
    students,
    refreshStudents,
-   searchingValue,
 }: Props) => {
+   const [pageNumber, setPageNumber] = useState(0)
+   const [userPerPage, setUserPerPage] = useState(25)
+
+   const pagesVisited = pageNumber * userPerPage
+   const pageCount = Math.ceil(students.length / userPerPage)
+   const changePage = ({ selected }) => {
+      setPageNumber(selected)
+   }
+
+   const studentsToDisplay = students
+      .slice(pagesVisited, pagesVisited + userPerPage)
+      .map((items) => (
+         <OneUser
+            layout={layout}
+            key={items.id}
+            student={items}
+            refreshStudents={refreshStudents}
+         />
+      ))
+
    return (
       <ResultsContainer>
-         {students
-            .filter((item) => {
-               return (
-                  item.student.firstName
-                     .toLowerCase()
-                     .includes(searchingValue) ||
-                  item.student.lastName.toLowerCase().includes(searchingValue)
-               )
-            })
-            .map((items) => (
-               <OneUser
-                  layout={layout}
-                  key={items.id}
-                  student={items}
-                  refreshStudents={refreshStudents}
-               />
-            ))}
+         {studentsToDisplay}
+         <div className="paginationBox">
+            <label>
+               <select onChange={(e) => setUserPerPage(Number(e.target.value))}>
+                  <option value="25">25</option>
+                  <option value="50">50</option>
+                  <option value="75">75</option>
+                  <option value="100">100</option>
+                  <option value={students.length}>Wszystkie</option>
+               </select>
+            </label>
+            <ReactPaginate
+               pageCount={pageCount}
+               previousLabel={<MdNavigateBefore />}
+               nextLabel={<MdNavigateNext />}
+               onPageChange={changePage}
+               previousClassName={'prevButton'}
+               nextClassName={'nextButton'}
+               containerClassName={'containerPagination'}
+               disabledClassName={'disabledPagination'}
+               activeClassName={'activePagination'}
+            />
+         </div>
       </ResultsContainer>
    )
 }
